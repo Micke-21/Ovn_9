cb9802363d5ad02dc8e1839d49b9fe540f1121f0
Mikael Larsson <micke.lar@gmail.com>
2022-01-13 12:05:55 +0100

    Bishbosh add check ned values, add mt

--- bishbosh.js
+++ bishbosh.js
@@ -13,14 +13,14 @@ btn.addEventListener('click', (e) => {
 
     let bb = BishBosh(bish.value, bosh.value, n.value);
 
-    if (div1 === null){
+    if (div1 === null) {
         let div1 = document.createElement("div");
         div1.setAttribute("id", "div1");
         div1.setAttribute("class", "bg-success bg-opacity-25 p-3");
         bishBoshSection.appendChild(div1);
         div1.innerHTML = bb;
     }
-    else{
+    else {
         div1.innerText = bb;
     }
 
@@ -28,6 +28,9 @@ btn.addEventListener('click', (e) => {
 
 function BishBosh(bish, bosh, n) {
     let res = '';
+    if (bish < 0) bish = 0;
+    if (bosh < 0) bosh = 0;
+    if (n < 0) n = 0;
 
     for (let index = 1; index <= n; index++) {
         if (index % bish == 0 && index % bosh == 0)
