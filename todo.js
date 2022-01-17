bac829e8a43c95d01377faccb3b5bc59172ad6db
Mikael Larsson <micke.lar@gmail.com>
2022-01-17 10:44:00 +0100

    Ny Js

--- todo.js
+++ todo.js
@@ -1,7 +1,9 @@
-const btn = document.querySelector('#btn');
 const todoDiv = document.querySelector('#todos');
-const todoRow = document.querySelector('#todos-1');
 const newTodo = document.querySelector('#todoInput');
+
+
+const btn = document.querySelector('#btn');
+const todoRow = document.querySelector('#todos-1');
 
 const p1 = document.querySelector('#p1');
 const b1 = document.querySelector('#b1');
@@ -11,25 +13,29 @@ const cb1 = document.querySelector('#cb-1');
 const ss = document.querySelectorAll('article > div > div > div').forEach(d =>
     d.addEventListener('click', (e) => {
         console.log('Klick av: ' + e)
+        
         d.classList.toggle('lth');
     }));
-
-todoRow.addEventListener('click', (e) => {
-    console.log('Checkbox: ' + cb1.checked);
-    p1.classList.toggle('lth');
-
-    // cb1.classList.toggle('lth');
-
-    if (cb1.checked)
+    console.log('s: ' + ss);
+    
+    todoRow.addEventListener('click', (e) => {
+        console.log('Checkbox: ' + cb1.checked);
+        p1.classList.toggle('lth');
+        
+        // cb1.classList.toggle('lth');
+        
+        if (cb1.checked)
         cb1.checked = false;
-    else
+        else
         cb1.checked = true;
+        
+    });
 
-});
 
 tb1.addEventListener('click', (e) => {
     todoRow.remove();
 });
+
 
 btn.addEventListener('click', (e) => {
     console.log('Button: ' + btn);
@@ -38,14 +44,26 @@ btn.addEventListener('click', (e) => {
 
     if (newTodo.value != "") {
         let newDiv = CreateDiv("row");
+        let inputField = CreateInput();
+        // inputField.addEventListener('click', (e) => {
+        //     if (inputField.checked)
+        //         inputField.checked = false;
+        //     else
+        //         inputField.checke = true;
+        // });
+
         let innerDiv = CreateDiv("col-sm-8")
-        innerDiv.appendChild(CreateInput());
+        innerDiv.appendChild(inputField);
         innerDiv.appendChild(createP());
+        innerDiv.addEventListener('click', (e) => {
+            console.log('InnerDiv Klick av: ' + e)
+            innerDiv.classList.toggle('lth');
+        });
 
         newDiv.appendChild(innerDiv);
 
-        newDiv.appendChild(createOkButton());
-        newDiv.appendChild(createRemoveButton());
+        newDiv.appendChild(createOkButton(innerDiv));
+        newDiv.appendChild(createRemoveButton(newDiv));
 
         todoDiv.appendChild(newDiv);
         newTodo.value = "";
@@ -58,19 +76,30 @@ btn.addEventListener('click', (e) => {
         let classatt = document.createAttribute("class");
         classatt.value = classValue;
         newDiv.setAttributeNode(classatt);
+
         return newDiv;
     }
 
     function CreateInput() {
-        let newInput = document.createElement("input");
-        newInput.innerHTML = newTodo.value;
+        let newInputField = document.createElement("input");
+        newInputField.innerHTML = newTodo.value;
+
         let att = document.createAttribute("type");
         att.value = "checkbox";
-        newInput.setAttributeNode(att);
+        newInputField.setAttributeNode(att);
+
         let classatt = document.createAttribute("class");
         classatt.value = "form-check-input me-3";
-        newInput.setAttributeNode(classatt);
-        return newInput;
+        newInputField.setAttributeNode(classatt);
+
+        newInputField.addEventListener('click', (e) => {
+            if (newInputField.checked)
+                newInputField.checked = false;
+            else
+                newInputField.checke = true;
+        });
+
+        return newInputField;
     }
 
     function createP() {
@@ -80,22 +109,32 @@ btn.addEventListener('click', (e) => {
     }
 });
 
-function createOkButton(){
+function createOkButton(newDiv) {
     let button = document.createElement("button");
     button.innerHTML = "ok";
     let classatt = document.createAttribute("class");
     classatt.value = "btn btn-primary col-sm-1";
     button.setAttributeNode(classatt);
 
+    button.addEventListener('click', (e) => {
+        console.log('Klick av: ' + e)
+
+        newDiv.classList.toggle('lth');
+    });
+
     return button;
 }
 
-function createRemoveButton(){
+function createRemoveButton(newDiv) {
     let button = document.createElement("button");
     button.innerHTML = "X";
     let classatt = document.createAttribute("class");
     classatt.value = "btn btn-danger col-sm-1";
     button.setAttributeNode(classatt);
+
+    button.addEventListener('click', (e) => {
+        newDiv.remove();
+    })
 
     return button;
 }
